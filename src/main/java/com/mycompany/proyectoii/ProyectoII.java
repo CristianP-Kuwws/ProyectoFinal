c5eb115ef2757db260fa2caa9fb5509039410505
hazky <hazky@LAPTOP-9RLN9ISB>
2025-04-01 19:08:22 -0400

    -Implementacion de conexion con BD mediante el metodo singlenton -Se agrego el JBDC al pom.xml para la conexion.

--- src/main/java/com/mycompany/proyectoii/ProyectoII.java
+++ src/main/java/com/mycompany/proyectoii/ProyectoII.java
@@ -5,7 +5,9 @@
 package com.mycompany.proyectoii;
 
 
+import Datos.ConexionBD;
 import GUI.VentanaPrincipal;
+import java.sql.Connection;
 
 /** 
  *
@@ -14,11 +16,32 @@ import GUI.VentanaPrincipal;
 public class ProyectoII {
 
     public static void main(String[] args) {
-        VentanaPrincipal ventana = new VentanaPrincipal();
-        ventana.setVisible(true);
+        
+        //VentanaPrincipal ventana = new VentanaPrincipal();
+        //ventana.setVisible(true);
         
         System.out.println("prueba de pull elliam.");
         
+        //Para comprobar si hubo conexion con BD (Pongan su info en ConexionBD.java)
+        
+        // obtener la instancia única de ConexionBD
+        ConexionBD conexionBD = ConexionBD.getInstancia();
+
+        // obtener la conexion
+        Connection conexion = conexionBD.getConexion();
+        
+        //Comprobar. 
+        
+        if (conexion != null) {
+            System.out.println("La conexión a la base de datos es exitosa.");
+        } else {
+            System.out.println("No se pudo establecer la conexión.");
+        }
+         
+        //Cerrar, quizas esta demas
+
+        conexionBD.cerrarConexion();
+    
         
     }
 }
