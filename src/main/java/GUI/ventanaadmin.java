19b1d59dfc9ea2c34f2e2f9b728ff59fa91fcf72
hazky <hazky@LAPTOP-9RLN9ISB>
2025-04-08 18:15:47 -0400

    - ...

--- src/main/java/GUI/ventanaadmin.java
+++ src/main/java/GUI/ventanaadmin.java
@@ -489,9 +489,7 @@ public class ventanaadmin extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(ventanaadmin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
-        //</editor-fold>
-
+        
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
